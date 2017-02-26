bb8de068ee51900d98acf1f88498b3af2742a7f7
Sean Whalley <swhalley@gmail.com>
2017-02-26 09:50:52 -0400

    Added Mapper file which will eventually replace the DAO

---
+++ src/main/java/root/data/ContactListMapper.java
@@ -0,0 +1,17 @@
+package root.data;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+@Mapper
+public interface ContactListMapper {
+
+    @Select( "SELECT * FROM Contacts" )
+    public List<String> getAllContacts();
+
+    @Select( "SELECT EXISTS(SELECT 1 FROM Contacts WHERE name=#{name})" )
+    public Boolean isContactInList(@Param("name") String name);
+}
