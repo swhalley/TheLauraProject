586bdce84fc78a2ba0078b74d2017ccbcafaa2ca
Sean Whalley <swhalley@gmail.com>
2017-02-26 10:38:21 -0400

    user select query updated to reflect table. removing dead file

--- src/main/java/root/rest/ContactEndpoints.java
+++ src/main/java/root/rest/ContactEndpoints.java
@@ -1,6 +1,5 @@
 package root.rest;
 
-import root.data.ContactListDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
