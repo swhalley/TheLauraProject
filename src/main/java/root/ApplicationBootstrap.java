681a2462455f0bcddb55c96bc28c7d8fb24ce4a3
Sean Whalley <swhalley@gmail.com>
2017-02-26 15:22:46 -0400

    starting to try some things with mybatis. no real progress

--- src/main/java/root/ApplicationBootstrap.java
+++ src/main/java/root/ApplicationBootstrap.java
@@ -3,9 +3,15 @@ package root;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import root.data.ContactListMapper;
 
 @SpringBootApplication
 public class ApplicationBootstrap extends SpringBootServletInitializer{
+    final private ContactListMapper mapper;
+
+    public ApplicationBootstrap(ContactListMapper mapper){
+        this.mapper = mapper;
+    }
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
