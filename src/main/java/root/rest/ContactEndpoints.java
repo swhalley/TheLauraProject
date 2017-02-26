099720cc306ce8b2802ccd489594466b0d88e7d7
Sean Whalley <swhalley@gmail.com>
2017-02-26 10:05:34 -0400

    introduce docker file for mongo db

--- src/main/java/root/rest/ContactEndpoints.java
+++ src/main/java/root/rest/ContactEndpoints.java
@@ -5,25 +5,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import root.data.ContactListMapper;
 
 import java.util.List;
 
 @RestController
 public class ContactEndpoints {
-    private ContactListDAO contactDAO;
+    private ContactListMapper dao;
 
     @Autowired
-    public ContactEndpoints(ContactListDAO contactDAO){
-        this.contactDAO = contactDAO;
+    public ContactEndpoints(ContactListMapper dao){
+        this.dao = dao;
     }
 
     @RequestMapping( "/all")
     public List<String> getContacts(){
-        return contactDAO.getAllContacts();
+        return dao.getAllContacts();
     }
 
     @RequestMapping( "/c")
     public String getContact( @RequestParam( value="name", defaultValue = "Sean") String name){
-        return contactDAO.isContactInList( name ) ? "Found him" : "keep looking";
+        return dao.isContactInList( name ) ? "Found him" : "keep looking";
     }
 }
